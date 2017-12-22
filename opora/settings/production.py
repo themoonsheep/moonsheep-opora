d816a4754138d5d208478446d142bc786aebc394
Piotr Peczek <ppeczek@gmail.com>
2017-12-22 18:31:24 +0100

    endpoint url hotfix

--- opora/settings/production.py
+++ opora/settings/production.py
@@ -13,7 +13,7 @@ DATABASES = {'default': dj_database_url.config()}
 MOONSHEEP_TASK_SOURCE = 'pybossa'
 
 # if pybossa is selected
-PYBOSSA_URL = 'https://crowdcrafting.org/api/'
+PYBOSSA_URL = 'https://crowdcrafting.org/'
 PYBOSSA_PROJECT_ID = 5115
 PYBOSSA_API_KEY = os.environ.get('PYBOSSA_API_KEY')
 
