276ec1b7088b279a7077a6994047df1e2d1997fc
Krzysztof Madejski <krzysztof@madejscy.pl>
2019-09-02 16:01:05 +0200

    TODOs

--- opora/settings/local.py
+++ opora/settings/local.py
@@ -6,6 +6,7 @@ SECRET_KEY = 'whateva'
 
 if DEBUG:
     INSTALLED_APPS += ['debug_toolbar', 'django_extensions']
+    # TODO reqs  pip install django-extensions, pip install django-debug-toolbar
     MIDDLEWARE.insert(1, 'debug_toolbar.middleware.DebugToolbarMiddleware')
 
 INTERNAL_IPS = [
